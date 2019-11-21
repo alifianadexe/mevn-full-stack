e06f059e869af7c392818e55063859afa1482a90
muhammad.aqshol <muhammad.aqshol@usg.co.id>
2019-11-21 15:39:54 +0700

    adding new view

---
+++ frontend/src/components/Posts.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="posts">
+        This will be list
+
+        <div v-for="post in posts" :key="post._id">
+            <p>
+                <span><b>{{ post.title }}</b></span><br>
+                <span>{{ post.description }}</span>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import PostsService from '@/services/PostsService';
+export default {
+    name:'posts',
+    data(){
+        return {
+            posts: []
+        }
+    },
+    mounted(){
+        this.getPosts()
+    },
+    methods: {
+        async getPosts(){
+            const response = await PostsService.fetchPost()
+            this.posts = response.data
+        }
+    }
+}
+</script>
--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
 export default() => {
-    return axios 
+    return axios.create({
+        baseURL:'http://localhost:8081'
+    })
 }
